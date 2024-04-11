9f3a8c5470b0d280b78a6e62a2e27c22e9ce5000
Amirabas Ahmadnia <59343626+amahmadnia@users.noreply.github.com>
2024-04-11 04:56:31 +0330

    added initial routes

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const DashboardPage = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      sss <Button>Click here</Button>
+    </main>
+  )
+}
+
+export default DashboardPage
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
-export default function Home() {
+const LandingPage = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      sss <Button>Click here</Button>
+      LANDING PAGE
     </main>
   )
 }
+
+export default LandingPage
