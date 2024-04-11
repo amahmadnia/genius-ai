4f03e4a1a6113788e8d8ba2c1e66369b51ff1e86
Amirabas Ahmadnia <59343626+amahmadnia@users.noreply.github.com>
2024-04-12 03:07:38 +0330

    adding sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const DashboardPage = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <UserButton>Click here</UserButton>
+      yo{' '}
     </main>
   )
 }
