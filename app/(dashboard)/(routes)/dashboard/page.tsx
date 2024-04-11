d82fb5dedc5c2cd85e61b17210f47beef469dccf
Amirabas Ahmadnia <59343626+amahmadnia@users.noreply.github.com>
2024-04-11 05:36:26 +0330

    fixed login loading

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 const DashboardPage = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      sss <Button>Click here</Button>
+      <UserButton>Click here</UserButton>
     </main>
   )
 }
