ef7c93432a96435c8c92dba5ec0eb6f2a72792db
Amirabas Ahmadnia <59343626+amahmadnia@users.noreply.github.com>
2024-04-12 03:37:17 +0330

    added sidebar mobile toggle function

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,12 +1,21 @@
 import { Menu } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import Sidebar from '@/components/sidebar'
 
 const MobileSidebar = () => {
   return (
-    <Button variant={'ghost'} className='md:hidden' size='icon'>
-      <Menu />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button variant={'ghost'} className='md:hidden' size='icon'>
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side='left' className='p-0'>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   )
 }
 
